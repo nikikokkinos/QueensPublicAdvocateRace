74664462c1a5812a3389fd8a6b4c7a62447f5ae5
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2019-04-12 15:43:51 -0400

    added tilelayer, setview & zoom, added geojson

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([40.674649,-73.844261], 11);
 
-L.tileLayer('https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var RaceResultsLayer = L.geoJson(RaceResults, {
+	fillColor: "purple",
+	fillOpacity: .02,
+	color: "#2b2e5e",
 }).addTo(map);
