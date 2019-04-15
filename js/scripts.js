7063e3268039395fb39403a7ef3354793c487595
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2019-04-15 14:48:37 -0400

    added successful switch statement funtion to color assembly districts by geoJson features

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,28 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// establishing geojson layer and setting fillColor to who won that district
 var RaceResultsLayer = L.geoJson(RaceResults, {
-	fillColor: "purple",
-	fillOpacity: .02,
-	color: "#2b2e5e",
-}).addTo(map);
+  style: function (geoJsonFeature) {
+    var winnerfillColor = geoJsonFeature.properties.QueensADs_Winner;
+
+    switch (winnerfillColor) {
+      case " Ulrich ":
+        winnerfillColor = "#b71500";
+        break;
+      case " Williams ":
+        winnerfillColor = "#0092b7";
+        break;
+      case " Kim ":
+         winnerfillColor = "#90a870";
+        break;
+      case " Viverito ":
+        winnerfillColor = "#a870a6";
+        break;
+      default:
+        winnerfillColor = "Black";
+        break;
+      }
+      return {color: "Black", weight: 1, fillColor: winnerfillColor, fillOpacity: 1};
+      }
+  }).addTo(map);
