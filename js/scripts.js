5af381a3d202f5a0b1259171b1b818cfffa69a0c
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2019-04-16 11:15:29 -0400

    scratched last code completely; redid everything and now the info div is updated on mouseover and mouseout; style also changes on mouseover and mouseout.

--- js/scripts.js
+++ js/scripts.js
@@ -4,86 +4,71 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// establishing geojson layer and setting fillColor to who won that district
-var RaceResultsLayer = L.geoJson(RaceResults, {
-  style: function (geoJsonFeature) {
-    var winnerfillColor = geoJsonFeature.properties.QueensADs_Winner;
+var geojson = L.geoJSON(RaceResults, {style: style, onEachFeature: onEachFeature}).addTo(map);
 
-    switch (winnerfillColor) {
-      case " Ulrich ":
-        winnerfillColor = "#b71500";
-        break;
-      case " Williams ":
-        winnerfillColor = "#0092b7";
-        break;
-      case " Kim ":
-         winnerfillColor = "#90a870";
-        break;
-      case " Viverito ":
-        winnerfillColor = "#a870a6";
-        break;
-      default:
-        winnerfillColor = "Black";
-        break;
-      }
-      return {color: "Black", weight: 2, fillColor: winnerfillColor, fillOpacity: 1};
-      },
-      onEachFeature: onEachFeature
-  }).addTo(map);
+function getfillColor(c) {
+    return c === ' Ulrich ' 	? '#cc0c0c' :
+           c === ' Williams ' ? '#1e1599' :
+           c === ' Viverito ' ? '#c80ccc' :
+           c === ' Kim '  		? '#0ccc58' :
+           c === ' Blake ' 		? '#f2e60c' :
+                      					'#b7b7b5';
+}
 
-  function highlightFeature(e) {
-      RaceResultsLayer = e.target;
-
-      info.update(RaceResultsLayer.feature.properties);
-
-      RaceResultsLayer.setStyle({
-        weight: 2,
-        color: '#666',
+function style(feature) {
+    return {
+        fillColor: getfillColor(feature.properties.QueensADs_Winner),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '2',
         fillOpacity: 0.7
-      });
+    };
+}
 
-      if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-          RaceResultsLayer.bringToFront();
-      }
-  }
+function highlight(e) {
+    var layer = e.target;
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+		info.update(layer.feature.properties);
+}
 
-  function zoomToFeature(e) {
-      map.fitBounds(e.target.getBounds());
-  }
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+		info.update();
+}
 
-  // creating a function that resets the map when user hovers out
-  function resetHighlight(e) {
-      RaceResultsLayer.resetStyle(e.target);
-
-      // updating custom control based on mouseout
-      info.update();
-  }
-
-  function onEachFeature (feature, RaceResultsLayer) {
-    RaceResultsLayer.on({
-        mouseover: highlightFeature,
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlight,
         mouseout: resetHighlight,
-        click: zoomToFeature,
-    })
-  }
+    });
+}
 
-  var info = L.control();
+var info = L.control();
 
-    info.onAdd = function (map) {
-       this._div = L.DomUtil.create('div', 'info');
-       this.update();
-       return this._div;
-   };
+  info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+  };
 
-    // method that we will use to update the control based on feature properties passed
-    info.update = function (props) {
-       this._div.innerHTML =
-            '<h4>2019 Public Advocate Special Election Results</h4>' +
-           (props ? '<b>' + 'Assembly District' + ' ' + props.AssemDist +
-           ' ' + ' - Votes Cast For - ' + '</b><br/>' + 'Ulrich' + ' ' + props.QueensADs_Ulrich +
-           '</b><br/>' + 'Williams' + ' ' + props.QueensADs_Williams + '</b><br/>' + 'Viverito' + ' ' +
-           props.QueensADs_Viverito + '</b><br/>' + 'Kim' + ' ' + props.QueensADs_Kim
-           : 'Hover over an Assembly District to view voting results');
-   };
+// method that we will use to update the control based on feature properties passed
+  info.update = function (props) {
+  this._div.innerHTML =
+    '<h4>2019 Public Advocate Special Election Results</h4>' +
+    (props ? '<b>' + 'Assembly District' + ' ' + props.AssemDist +
+    ' ' + ' - Votes Cast For - ' + '</b><br/>' + 'Ulrich' + ' ' + props.QueensADs_Ulrich +
+    '</b><br/>' + 'Williams' + ' ' + props.QueensADs_Williams + '</b><br/>' + 'Viverito' + ' ' +
+    props.QueensADs_Viverito + '</b><br/>' + 'Kim' + ' ' + props.QueensADs_Kim
+    : 'Hover over an Assembly District to view voting results');
+  };
 
-   info.addTo(map);
+info.addTo(map);
