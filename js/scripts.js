612d07552059eb83f49fd28a09f223829ac9f1f8
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2019-04-17 11:18:47 -0400

    added voterturnout layer to map, styled such layer, added onEachFeature interaction, added info div for mouseover and mouseout events

--- js/scripts.js
+++ js/scripts.js
@@ -76,13 +76,100 @@ var info = L.control();
   };
 
 info.addTo(map);
-// 
-// var legend = L.DomUtil.create('div', 'legend'),
-//     grades = [
-// 			' Ulrich ',
-// 			' Williams ',
-// 			' Viverito ',
-// 			' Koo ',
-// 		],
-//     labels = [],
-//     from, to;
+
+var TotalVotesLayer = L.geoJSON(RaceResults, {style: votesStyle, onEachFeature: votesonEachFeature})
+
+function votesfillColor(v) {
+    return v >= 10000 	? '#0c2c84' :
+					 v >= 9000 		? '#225ea8' :
+           v >= 8000 		? '#1d91c0' :
+					 v >= 7000 		? '#41b6c4' :
+           v >= 6000		? '#7fcdbb' :
+					 v >= 5000		? '#c7e9b4' :
+					 v >= 4000		? '#edf8b1' :
+					 v >= 3000		? '#ffffd9' :
+					 								'black' ;
+}
+
+function votesStyle(featurez) {
+    return {
+        fillColor: votesfillColor(featurez.properties.QueensADs_TotalVotes),
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '2',
+        fillOpacity: 0.7
+    };
+}
+
+function highlight2(e) {
+    var layer = e.target;
+    layer.setStyle({
+        weight: 2.5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+		voterturnoutinfo.update(layer.feature.properties);
+}
+
+function resetHighlight2(e) {
+    TotalVotesLayer.resetStyle(e.target);
+		voterturnoutinfo.update();
+}
+
+function zoomToFeature2(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function votesonEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlight2,
+        mouseout: resetHighlight2,
+				click: zoomToFeature2,
+    });
+}
+
+var voterturnoutinfo = L.control();
+
+voterturnoutinfo.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'voterturnoutinfo'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+voterturnoutinfo.update = function (props) {
+    this._div.innerHTML = '<h4>Voter Turnout by Assembly District</h4>' +  (props ?
+        '<b>' + 'Assembly District' + ' ' + props.AssemDist + '</b><br />' + props.QueensADs_TotalVotes
+        : 'Hover over an Assembly District');
+};
+
+voterturnoutinfo.addTo(map);
+
+$('.voterturnoutinfo').hide()
+
+var baselayer = {
+	"Who Won each AD": WinnerLayer,
+	"Voter Turnout": TotalVotesLayer,
+}
+
+L.control.layers(baselayer).addTo(map);
+
+map.on('baselayerchange', function(eventLayer) {
+  if (eventLayer.name === 'Who Won each AD'){
+  $('.info').show()
+  }
+  if (eventLayer.name === 'Who Won each AD'){
+  $('.voterturnoutinfo').hide()
+  }
+  if (eventLayer.name === 'Voter Turnout'){
+  $('.voterturnoutinfo').show()
+  }
+  if (eventLayer.name === 'Voter Turnout'){
+  $(".info").hide()
+  }
+});
