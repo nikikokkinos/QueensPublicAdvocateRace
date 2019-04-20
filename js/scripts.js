07f36e182ffeef2d6fd0d085ed60f23d399511b0
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2019-04-20 12:32:56 -0400

    cleaned code readability

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,19 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// establishing external geoJSON file & giving it two arguments for style and onEachFeature
 var WinnerLayer = L.geoJSON(RaceResults, {style: style, onEachFeature: onEachFeature}).addTo(map);
 
-function getfillColor(c) {
-    return c === ' Ulrich ' 	? '#cc0c0c' :
-           c === ' Williams ' ? '#35a7ff' :
-           c === ' Viverito ' ? '#c80ccc' :
-           c === ' Kim '  		? '#0ccc58' :
-                      					'#b7b7b5';
+// function to color each AD by QueensADs_Winner property
+function getfillColor(QueensADs_Winner) {
+    return QueensADs_Winner === ' Ulrich '		? '#cc0c0c' :
+           QueensADs_Winner === ' Williams '	? '#35a7ff' :
+           QueensADs_Winner === ' Viverito '	? '#c80ccc' :
+           QueensADs_Winner === ' Kim '				? '#0ccc58' :
+                      													'#b7b7b5';
 }
 
+// funtion to style each AD by the above getfillColor function
 function style(feature) {
     return {
         fillColor: getfillColor(feature.properties.QueensADs_Winner),
@@ -25,6 +28,7 @@ function style(feature) {
     };
 }
 
+// function to change each AD style on mouseover
 function highlight(e) {
     var layer = e.target;
     layer.setStyle({
@@ -36,18 +40,22 @@ function highlight(e) {
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
     }
+		// updating info div created below on mouseover
 		info.update(layer.feature.properties);
 }
 
+// function to change each AD style on mouseout
 function resetHighlight(e) {
     WinnerLayer.resetStyle(e.target);
 		info.update();
 }
 
+// function to zoom to each AD on click
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
 
+// onEachFeature funtion that iterates all above functions
 function onEachFeature(feature, layer) {
     layer.on({
         mouseover: highlight,
@@ -56,6 +64,7 @@ function onEachFeature(feature, layer) {
     });
 }
 
+// creating control that changes innerHTML based on mouse events
 var info = L.control();
 
   info.onAdd = function (map) {
@@ -77,6 +86,7 @@ var info = L.control();
 
 info.addTo(map);
 
+// creating new control that loops over the QueensADs_Winner array
 var winners, wLen, legend;
 
 winners = [' Ulrich ', ' Williams ', ' Viverito ', ' Kim '];
@@ -95,23 +105,24 @@ legend.onAdd = function () {
 
 legend.addTo(map);
 
+// same code structure as above, but applied to a new layer
 var TotalVotesLayer = L.geoJSON(RaceResults, {style: votesStyle, onEachFeature: votesonEachFeature})
 
-function votesfillColor(v) {
-    return v >= 10000 	? '#0c2c84' :
-					 v >= 9000 		? '#225ea8' :
-           v >= 8000 		? '#1d91c0' :
-					 v >= 7000 		? '#41b6c4' :
-           v >= 6000		? '#7fcdbb' :
-					 v >= 5000		? '#c7e9b4' :
-					 v >= 4000		? '#edf8b1' :
-					 v >= 3000		? '#ffffd9' :
+function votesfillColor(QueensADs_TotalVotes) {
+    return QueensADs_TotalVotes >= 10000 	? '#0c2c84' :
+					 QueensADs_TotalVotes >= 9000 	? '#225ea8' :
+           QueensADs_TotalVotes >= 8000 	? '#1d91c0' :
+					 QueensADs_TotalVotes >= 7000 	? '#41b6c4' :
+           QueensADs_TotalVotes >= 6000		? '#7fcdbb' :
+					 QueensADs_TotalVotes >= 5000		? '#c7e9b4' :
+					 QueensADs_TotalVotes >= 4000		? '#edf8b1' :
+					 QueensADs_TotalVotes >= 3000		? '#ffffd9' :
 					 								'black' ;
 }
 
-function votesStyle(featurez) {
+function votesStyle(feature) {
     return {
-        fillColor: votesfillColor(featurez.properties.QueensADs_TotalVotes),
+        fillColor: votesfillColor(feature.properties.QueensADs_TotalVotes),
         weight: 1,
         opacity: 1,
         color: 'black',
@@ -193,13 +204,16 @@ voterturnoutlegend.addTo(map);
 //
 $('.voterturnoutlegend').hide();
 
+// grouping together the layers
 var baselayers = {
 	"Who Won each AD": WinnerLayer,
 	"Voter Turnout": TotalVotesLayer,
 }
 
+// creating control for layers
 L.control.layers(baselayers).addTo(map);
 
+// function that shows & hides info divs based on which layer is on
 map.on('baselayerchange', function(eventLayer) {
   if (eventLayer.name === 'Who Won each AD'){
   	$('.info').show()
@@ -226,3 +240,25 @@ map.on('baselayerchange', function(eventLayer) {
 		$(".legend").hide()
 	}
 });
+
+// array of urls associated w/ each AssemDist
+// var assemblyWebsites = [
+// 	"https://nyassembly.gov/mem/Aravella-Simotas",
+// 	"https://nyassembly.gov/mem/Catherine-Nolan",
+// 	"https://nyassembly.gov/mem/Jeffrion-L-Aubry",
+// 	"https://nyassembly.gov/mem/Michael-G-DenDekker",
+// 	"https://nyassembly.gov/mem/Catalina-Cruz",
+// 	"https://nyassembly.gov/mem/Brian-Barnwell",
+// 	"https://nyassembly.gov/mem/Andrew-Hevesi",
+// 	"https://nyassembly.gov/mem/Michael-Miller",
+// 	"https://nyassembly.gov/mem/Stacey-Pheffer-Amato",
+// 	"https://nyassembly.gov/mem/David-I-Weprin",
+// 	"https://nyassembly.gov/mem/Daniel-Rosenthal",
+// 	"https://nyassembly.gov/mem/Nily-Rozic",
+// 	"https://nyassembly.gov/mem/Ron-Kim",
+// 	"https://nyassembly.gov/mem/Edward-C-Braunstein",
+// 	"https://nyassembly.gov/mem/Clyde-Vanel",
+// 	"https://nyassembly.gov/mem/Alicia-Hyndman",
+// 	"https://nyassembly.gov/mem/Vivian-E-Cook",
+// 	"https://nyassembly.gov/mem/Michele-R-Titus",
+// ];
